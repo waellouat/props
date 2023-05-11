e601c9d6c528fa4b8512376c1ab2e07c9e6aecc8
waellouat <116644467+waellouat@users.noreply.github.com>
2023-05-11 10:38:06 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import React from "react";
 import FullName from "./profile/FullName";
 import Bio from "./profile/Bio";
-import Profssion from "./profile/Profssion";
+import Profession from "./profile/Profession";
 import HandleName from "./profile/HandleName";
+
 function App() {
-  const handleEvent =(t) => {
-    alert('test ${t} ');
-  }
+  const handleEvent = (name) => {
+    alert(`Name: ${name}`);
+  };
+
   return (
     <div className="App">
-     <body style= {{margin:"150px 100px",fontSize:"1.2rem"}} >
-<h1> my profile</h1>
-<FullName FirstName="wael" LastName="louati" >
-<p>hi i am wael louati</p>
-</FullName>
-<br/>
-<Bio/>
-<div id="profession"><Profssion/></div>
-<HandleName handleEvent={handleEvent} FirstName="wael">
-
-</HandleName>
-     </body>
+      <body style={{ margin: "150px 100px", fontSize: "1.2rem" }}>
+        <h1>My Profile</h1>
+        <FullName FirstName="Wael" LastName="Louati">
+          <p>Hi, I am Wael Louati</p>
+        </FullName>
+        <br />
+        <Bio />
+        <div id="profession">
+          <Profession />
+        </div>
+        <HandleName handleEvent={handleEvent} FirstName="Wael" />
+      </body>
     </div>
   );
 }
 
 export default App;
+
